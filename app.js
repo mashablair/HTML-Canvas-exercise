38514123bf4ac597bfeaf35fc72e91494842ff5e
Maria Blair <maria.klachko@gmail.com>
2016-08-12 15:34:32 -0700

    1st commit: created two practice files for my HTML Canvas class.

---
+++ app.js
@@ -0,0 +1,37 @@
+
+
+// $(function(){
+//     $('#puppy').attr('src', 'http://lorempixel.com/350/150/animals/');
+// })
+
+// $( 'article' ).on( 'click', function( evt ) {
+//     $( evt.target ).css( 'background', 'red' );
+// });
+
+
+// OBJECT-ORIENTED JS CLASS:
+
+// var Car = function(loc) {
+//     var obj = Object.create(Car.prototype);
+//     obj.loc = loc;
+//     return obj;
+// };
+// Car.prototype.move = function () {
+//     this.loc++;
+// };
+
+// var amy = Car(1);
+// amy.move();
+// var ben = Car(9);
+// ben.move();
+
+
+// var Dog = function() {
+//     return {legs: 4, bark: alert};
+// };
+// var fido=Dog();
+
+// log(fido instanceof Dog);
+
+
+
